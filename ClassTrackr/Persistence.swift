6afb921e769470b564397e827d73d56fb7913111
Andrius Shiaulis <shiaulis@gmail.com>
2025-05-24 23:42:41 +0300

    Integrates SwiftLint configuration
    Adds a SwiftLint configuration file with custom rules and automated code analysis options.
    
    Enhances code quality by enabling various linting rules and setting exclusions.
    
    Updates project configuration to include SwiftLint plugins as a package dependency.

--- ClassTrackr/Persistence.swift
+++ ClassTrackr/Persistence.swift
@@ -8,11 +8,11 @@
 import CoreData
 
 struct PersistenceController {
-    static let shared = PersistenceController()
+    static let shared = Self()
 
     @MainActor
-    static let preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
+    static let preview: Self = {
+        let result = Self(inMemory: true)
         let viewContext = result.container.viewContext
         for _ in 0..<10 {
             let newItem = Item(context: viewContext)
@@ -20,7 +20,8 @@ struct PersistenceController {
         }
         do {
             try viewContext.save()
-        } catch {
+        }
+        catch {
             // Replace this implementation with code to handle the error appropriately.
             // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             let nsError = error as NSError
@@ -36,7 +37,7 @@ struct PersistenceController {
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores(completionHandler: { _, error in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
